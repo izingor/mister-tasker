b46b61d6de90e6cf1a63ffe0a977cbc4c166a5db
Igor <izingor@gmail.com>
2022-04-07 15:04:45 +0300

    removed middlewares in the task route

--- backend/api/task/task.routes.js
+++ backend/api/task/task.routes.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const { requireAuth } = require('../../middlewares/requireAuth.middleware')
-const { log } = require('../../middlewares/logger.middleware')
-const { getOrders, getOrderById, addOrder, updateOrder, removeOrder } = require('./task.controller')
-const router = express.Router()
+const express = require('express');
+const { requireAuth } = require('../../middlewares/requireAuth.middleware');
+const { log } = require('../../middlewares/logger.middleware');
+const { getOrders, getOrderById, addOrder, updateOrder, removeOrder } = require('./task.controller');
+const router = express.Router();
 
 // middleware that is specific to this router
 // router.use(requireAuth)
 
-router.get('/', log, getOrders)
-router.get('/:id', getOrderById)
+router.get('/', log, getOrders);
+router.get('/:id', getOrderById);
 // router.post('/', addOrder)
 // router.put('/:id', updateOrder)
 // router.delete('/:id', removeOrder)
-router.post('/', requireAuth, addOrder)
-router.put('/:id', requireAuth, updateOrder)
-router.delete('/:id', requireAuth, removeOrder)
+router.post('/', addOrder);
+router.put('/:id', updateOrder);
+router.delete('/:id', removeOrder);
 
-module.exports = router
+module.exports = router;
